32d8fb713ee3687eca40058874ce7acc05853322
张敏超🍎 <zhangminchao@sensorsdata.cn>
2020-02-23 10:56:47 +0800

    Init

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 张敏超🍎 on 2020/2/23.
+//  Copyright © 2020 zhangminchao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HookDelegate/HookDelegate/HookDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HookDelegate.h
+//  HookDelegate
+//
+//  Created by 张敏超🍎 on 2020/2/23.
+//  Copyright © 2020 zhangminchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HookDelegate.
+FOUNDATION_EXPORT double HookDelegateVersionNumber;
+
+//! Project version string for HookDelegate.
+FOUNDATION_EXPORT const unsigned char HookDelegateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HookDelegate/PublicHeader.h>
+
+
